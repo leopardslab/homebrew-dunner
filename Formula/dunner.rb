73afceeb5ba212a358896fd8929895307fd8368b
goreleaserbot <goreleaser@carlosbecker.com>
2019-07-06 14:36:45 +0530

    Brew formula update for dunner version v2.1.3

--- Formula/dunner.rb
+++ Formula/dunner.rb
@@ -2,9 +2,15 @@
 class Dunner < Formula
   desc "A Docker based task runner tool"
   homepage "https://github.com/leopardslab/Dunner"
-  url "https://github.com/leopardslab/dunner/releases/download/v2.1.1/dunner_2.1.1_darwin_x86_64.tar.gz"
-  version "2.1.1"
-  sha256 "fc1d0831264a30d1e3189db36ceeb0f8240c5f6489cec0e388bc8d87b98a4f86"
+  version "2.1.3"
+
+  if OS.mac?
+    url "https://github.com/leopardslab/dunner/releases/download/v2.1.3/dunner_2.1.3_darwin_x86_64.tar.gz"
+    sha256 "8487fc26a116071cf060d55acb61dc32f43b2e0b44961ad55a7317eb5f6dafa5"
+  elsif OS.linux?
+    url "https://github.com/leopardslab/dunner/releases/download/v2.1.3/dunner_2.1.3_linux_x86_64.tar.gz"
+    sha256 "d5f7b402d9de46e07091c117be2ed22380b668beedaa9947d75240ab7b6794d3"
+  end
 
   def install
     bin.install "dunner"
